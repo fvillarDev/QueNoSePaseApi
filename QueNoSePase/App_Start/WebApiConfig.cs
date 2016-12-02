785f3245e4d58205e6c96340e4d909fc8fdb7e61
Facu Villar <facuandroid.fv@gmail.com>
2016-12-01 22:41:15 -0300

    Enable Cors App Start Config

--- QueNoSePase/App_Start/WebApiConfig.cs
+++ QueNoSePase/App_Start/WebApiConfig.cs
@@ -10,6 +10,8 @@ namespace QueNoSePase.API
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
+            config.EnableCors();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
